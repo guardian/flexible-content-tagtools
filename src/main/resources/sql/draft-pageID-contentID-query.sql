3ef801b46c30afa6368507da834bb660a731a3a4
alessandro ludovici <alessandro.ludovici@guardian.com>
2014-12-09 11:39:28 +0000

    Updated query to add lastModified to R2Cache

--- src/main/resources/sql/draft-pageID-contentID-query.sql
+++ src/main/resources/sql/draft-pageID-contentID-query.sql
@@ -1,4 +1,4 @@
-select p.id as page_id, c.id as content_id
+select p.id as page_id, c.id as content_id, p.LAST_MODIFIED_ID as last_modified
 from page_draft p
 inner join content_draft c ON p.CORE_CONTENT_ID = c.id
 inner join resource_content_draft rcl on rcl.id = c.id
