6b1d536d2481c2368613d3f60aa8c041139e29bb
alessandro ludovici <alessandro.ludovici@guardian.com>
2014-12-09 16:58:34 +0000

    Update unit tests

--- src/test/scala/com/gu/tagdiffer/unitTests/DiffFunctionTests.scala
+++ src/test/scala/com/gu/tagdiffer/unitTests/DiffFunctionTests.scala
@@ -15,14 +15,15 @@ class DiffFunctionTests extends FeatureSpec with GivenWhenThen with ShouldMatche
   val publicationTag = Tag(5, Publication, "tag 6", "tag", None, section, isLead = false, true)
   val bookTag = Tag(6, Book, "tag 7", "tag", None, section, isLead = false, true)
   val bookSectionTag = Tag(7, BookSection, "tag 8", "tag", None, section, isLead = false, true)
-  val timestamp = new DateTime()
+  val createTimestamp = new DateTime()
+  val lastModified = new DateTime()
 
   feature("Check tags order") {
     scenario("different order") {
       given("the same tags")
         val flexiTags = FlexiTags(List(mainTag, leadTag), List(contributorTag), List(publicationTag), List(bookTag), List(bookSectionTag))
         val r2Tags = R2Tags(List(leadTag, mainTag, contributorTag, publicationTag, bookSectionTag, bookTag))
-        val content = Content("1", "11", "article", timestamp, flexiTags, r2Tags)
+        val content = Content("1", "11", "article", createTimestamp, lastModified, lastModified, flexiTags, r2Tags)
       then("different order should be true")
       and("different number should be 0")
         content.diffTags(_.allTags) should be((true, 0))
@@ -32,7 +33,7 @@ class DiffFunctionTests extends FeatureSpec with GivenWhenThen with ShouldMatche
       given("the same tags")
       val flexiTags = FlexiTags(List(mainTag, leadTag), List(contributorTag), List(publicationTag), List(bookTag), List(bookSectionTag))
       val r2Tags = R2Tags(List(mainTag, leadTag, contributorTag, publicationTag, bookTag, bookSectionTag))
-      val content = Content("1", "11", "article", timestamp, flexiTags, r2Tags)
+      val content = Content("1", "11", "article", createTimestamp, lastModified, lastModified, flexiTags, r2Tags)
       then("different order should be false")
       and("different number should be 0")
         content.diffTags(_.allTags) should be((false, 0))
@@ -44,7 +45,7 @@ class DiffFunctionTests extends FeatureSpec with GivenWhenThen with ShouldMatche
       given("4 tags for flexi and 2 for R2")
       val flexiTags = FlexiTags(List(leadTag), List(contributorTag), List(), List(bookTag), List(bookSectionTag))
       val r2Tags = R2Tags(List(mainTag, contributorTag2))
-      val content = Content("1", "11", "article", timestamp, flexiTags, r2Tags)
+      val content = Content("1", "11", "article", createTimestamp, lastModified, lastModified, flexiTags, r2Tags)
       then("different number should be 6")
       content.diffTags(_.allTags) should be((false, 6))
     }
@@ -54,7 +55,7 @@ class DiffFunctionTests extends FeatureSpec with GivenWhenThen with ShouldMatche
       and("both R2 tags are in flexi")
       val flexiTags = FlexiTags(List(leadTag), List(contributorTag), List(), List(bookTag), List(bookSectionTag))
       val r2Tags = R2Tags(List(leadTag, contributorTag))
-      val content = Content("1", "11", "article", timestamp, flexiTags, r2Tags)
+      val content = Content("1", "11", "article", createTimestamp, lastModified, lastModified, flexiTags, r2Tags)
       then("different number should be 2")
       content.diffTags(_.allTags) should be((false, 2))
     }
@@ -63,7 +64,7 @@ class DiffFunctionTests extends FeatureSpec with GivenWhenThen with ShouldMatche
       given("4 tags for r2")
       val flexiTags = FlexiTags(List(), List(), List(), List(), List())
       val r2Tags = R2Tags(List(leadTag, contributorTag, publicationTag, bookTag))
-      val content = Content("1", "11", "article", timestamp, flexiTags, r2Tags)
+      val content = Content("1", "11", "article", createTimestamp, lastModified, lastModified, flexiTags, r2Tags)
       then("different number should be 4")
       content.diffTags(_.allTags) should be((false, 4))
     }
@@ -72,7 +73,7 @@ class DiffFunctionTests extends FeatureSpec with GivenWhenThen with ShouldMatche
       given("4 tags for flexi")
       val flexiTags = FlexiTags(List(leadTag), List(contributorTag), List(publicationTag), List(bookTag), List())
       val r2Tags = R2Tags(List())
-      val content = Content("1", "11", "article", timestamp, flexiTags, r2Tags)
+      val content = Content("1", "11", "article", createTimestamp, lastModified, lastModified, flexiTags, r2Tags)
       then("different number should be 4")
       content.diffTags(_.allTags) should be((false, 4))
     }
@@ -80,7 +81,7 @@ class DiffFunctionTests extends FeatureSpec with GivenWhenThen with ShouldMatche
     scenario("R2 and Flexi have no tags") {
       val flexiTags = FlexiTags(List(), List(), List(), List(), List())
       val r2Tags = R2Tags(List())
-      val content = Content("1", "11", "article", timestamp, flexiTags, r2Tags)
+      val content = Content("1", "11", "article", createTimestamp, lastModified, lastModified, flexiTags, r2Tags)
       then("different number should be 0")
       content.diffTags(_.allTags) should be((false, 0))
     }
