cf6966a6ee6f6f93d8e85e6621a1234886fcd6b6
Christopher Lloyd <christopher.lloyd@theguardian.com>
2016-02-23 12:37:15 +0000

    Fix Oracle SQL queries

--- src/main/resources/sql/tag-audit-merged-tags-query.sql
+++ src/main/resources/sql/tag-audit-merged-tags-query.sql
@@ -1,2 +1,2 @@
-select new_tag_id, old_tag_id from tag_audit where performed_on > to_date('02-OCT-11','DD-MON-YY') and operation = "merge"
+select new_tag_id, old_tag_id from tag_audit where performed_on > to_date('02-OCT-11','DD-MON-YY') and operation = 'merge'
 order by performed_on;
